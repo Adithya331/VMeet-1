2637e7330ebede4a04b46d970e9e1d4ad61d17d9
clocking <velaparthiadithya@gmail.com>
2023-04-19 16:36:37 +0530

    updated login and sigin page

--- vmeet/src/App.js
+++ vmeet/src/App.js
@@ -1,10 +1,26 @@
+import React from 'react';
+import { Route,Routes } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 //import logo from './logo.svg';
 //import './App.css';
+//import Landing from './Landing';
 import Landing from './Landing';
+import Login from './Login';
+import Signup from './Signup';
 
 function App() {
   return (
-    <Landing/>
+      <Router>
+      <div>
+        <section>                              
+            <Routes>                                                                        
+               <Route path="/" element={<Landing/>}/>
+               <Route path="/login" element={<Login/>}/>
+               <Route path="/signup" element={<Signup/>}/>
+            </Routes>                    
+        </section>
+      </div>
+    </Router>
   );
 }
 
